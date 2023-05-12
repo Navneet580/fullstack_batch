0a55c8c202aa5d05d3c39be14acdedcbb1371abd
Navneet Jaiswal <navneetjaiswal69@gmail.com>
2023-05-12 16:06:33 +0530

    watch Navneet file

--- By_Navneet-ToDo_List/script.js
+++ By_Navneet-ToDo_List/script.js
@@ -2,76 +2,62 @@
 
 const todoContainer = document.querySelector('ul');
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
+const addButton = document.querySelector("#add");
+const editButton = document.querySelector("#edit");
 
-// **********1
-// const handleAdd = (e) => {
-//     console.log('adding item')
-//     // console.log(input.innerHTML)
-//     console.log(input.value)
-// }
-// const todos = []
-// button.addEventListener('click', handleAdd)
+editButton.style = "display: none";
 
-// ***********2
-// const handleAdd = (e)=> {
-//     const itemToAdd = input.value
-//     if (itemToAdd!==null && itemToAdd!=='') {
-//         todos.push({
-//             text: itemToAdd,
-//             completed: false,
-//             id: Date.now()
-//         })
-//         console.log(todos)
-//         return
-//     } else {
-//         return window.alert('Adding an empty item is not possible.')
-//     }
-// }
-// const todos = []
-// button.addEventListener('click',handleAdd)
+let idOfElementToEdit = null;
 
+const todos = [];
 
-
-// ********************************************
-const todos = []
-// **********
-
-// const renderList = () => {
-//     todoContainer.innerHTML = ''
-//     todos.forEach((todo)=>{
-//         const li = document.createElement('li')
-//         // const button = document.createElement('button')
-//         // button.innerHTML = 'delete'
-//         li.innerHTML = todo.text
-//         todoContainer.appendChild(li)
-//     })
-// }
-
+const showEdit = (id)=> {
+    const element = todos.find((todo) => todo.id === id);
+    input.value = element.text;
+    addButton.style = "display: none";
+    editButton.style = "display: inline";
+    idOfElementToEdit = id;
+};
 
 const renderList = ()=>{
     todoContainer.innerHTML = ''
     todos.forEach((todo)=>{
         const li = document.createElement('li')
-        const button = document.createElement('button')
-        button.addEventListener('click',(e)=>{
-            li.remove()
-            
-            // const id = todo.id
-            // todos.splice(todos.findIndex((todo)=> todo.id===id),1)
+        const deleteButton = document.createElement('button')
+        const editButton = document.createElement('button')
 
+        deleteButton.addEventListener('click',(e)=>{
+            li.remove()
+            const id = todo.id
+            // todos.splice(todos.findIndex((todo)=> todo.id===id),1)
+            const index = todos.findIndex((todo)=> todo.id===id)
+            todos.splice(index,1)
         })
 
-        button.innerHTML = 'delete'
+        editButton.addEventListener('click',(e)=>{
+            showEdit(todo.id)
+        })
+
+        deleteButton.style = "background:red";
+        editButton.style = "background:yellow";
+
+        deleteButton.innerHTML = 'Delete';
+        editButton.innerHTML = 'Edit';
+
         li.innerHTML = `
         ${todo.text} 
-        `
-        li.appendChild(button)
-        todoContainer.appendChild(li)
+        `;
+
+        li.appendChild(deleteButton);
+        li.appendChild(editButton);
+        todoContainer.appendChild(li);
     })
-}
+    
+};
 
 const handleAdd = (e)=>{
+    
     const itemToAdd = input.value
     if(itemToAdd!==null && itemToAdd!==''){
         todos.push({
@@ -79,12 +65,24 @@ const handleAdd = (e)=>{
             completed: false,
             id: Date.now()
         })
-    // console.log(todos)
+    
+        input.value=""
     renderList()
         return 
     }else{
        return window.alert('adding an empty item is not possible')
     }
-}
+};
 
-button.addEventListener('click',handleAdd)
+const handleEdit = (e) => {
+    const newValue = input.value;
+    const element = todos.find((todo) => todo.id === idOfElementToEdit);
+    element.text = newValue;
+    renderList();
+    addButton.style = "display: inline";
+    editButton.style = "display: none";
+    input.value = "";
+};
+
+addButton.addEventListener('click', handleAdd)
+editButton.addEventListener('click', handleEdit)
