4347ec0b07fdf6f7f56661e9a21986896de40a33
Navneet Jaiswal <navneetjaiswal69@gmail.com>
2023-05-12 17:47:01 +0530

    node js dependencies

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+
+// const {v4: uuidv4} = require('uuid');
+// const fetch = require('node-fetch')
+
+// // console.log(fetch)
+
+// const id = uuidv4();
+// console.log(id)
+
+// const fetchData = async()=>{
+//     try {
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+//         const data = await res.json()
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// fetchData()
+
+// *****
+// console.log('radhe')
+
+// for (let i=0; i<10; i++) {
+//     console.log(i)
+// }
+
+// *******
+
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs')
 
 const PORT = 8080
 
